e25045149d72b336a635c204e71ffc7fe2861143
APPTEST <s007salim@yahoo.com>
2025-08-31 13:32:52 +0800

    Added a second print statement

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+def say_hello():
+    return "Hello from my first Git upload!"
+
+if __name__ == "__main__":
+    print(say_hello())
+    print("This is my second version!")   # <-- New line
+
+
+
 # This is a test change for Dev CI
 def say_hello():
     return "Hello, CI/CD!"
